5354fac0af623c40cad41b0027919f822b4c6c04
Steeven Toala <steevenpfb@gmail.com>
2024-09-13 15:30:30 -0500

    Correccion del fornt de INICIO Y REGISTRO

--- index.php
+++ index.php
@@ -8,30 +8,9 @@
     <title>Home</title>    
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Mi Sitio</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Servicios</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contacto</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="pages/iniciarSesion.php">Iniciar Sesion</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+    <?php
+        include '../util/menuindex.php'
+    ?>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
---
+++ util/conexion.php
@@ -0,0 +1,10 @@
+<?php
+$hostname="localhost";
+$username="root";
+$password="";
+$database="prog";
+
+$conexion = mysqli($hostname,$username,$password,$database);
+
+
+?>
