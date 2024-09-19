8cf674ae2f9833028c4de8378692e575e90ee86f
Steeven Toala <steevenpfb@gmail.com>
2024-09-19 15:30:58 -0500

    Agregacion de archivo database.sql prueba alert button Registro

---
+++ database/database.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users(
+id int AUTO_INCREMENT PRIMARY KEY,
+email varchar(50),
+password varchar(50)
+);
---
+++ js/iniciarSesion.js
@@ -0,0 +1,12 @@
+  let btnregistrarse = document.getElementById("registrarse");
+      let inputEmail = document.getElementById("email");
+      let inputPassword = document.getElementById("password");
+
+      btnregistrarse.addEventListener('click',verAlerta,false);
+
+      function verAlerta(){      
+        inputEmail.value="";
+        inputPassword.value="";
+        alert('Ingresa valores validos');
+      }
+     
