0d089cad2d9229e5e1d5193989f2149ac94ac68d
Steeven Toala <steevenpfb@gmail.com>
2024-09-19 15:33:40 -0500

    formato a los documentos php js

--- js/iniciarSesion.js
+++ js/iniciarSesion.js
@@ -1,12 +1,11 @@
-  let btnregistrarse = document.getElementById("registrarse");
-      let inputEmail = document.getElementById("email");
-      let inputPassword = document.getElementById("password");
+let btnregistrarse = document.getElementById("registrarse");
+let inputEmail = document.getElementById("email");
+let inputPassword = document.getElementById("password");
 
-      btnregistrarse.addEventListener('click',verAlerta,false);
+btnregistrarse.addEventListener('click', verAlerta, false);
 
-      function verAlerta(){      
-        inputEmail.value="";
-        inputPassword.value="";
-        alert('Ingresa valores validos');
-      }
-     
+function verAlerta() {
+  inputEmail.value = "";
+  inputPassword.value = "";
+  alert('Ingresa valores validos');
+}
--- pages/registrarse.php
+++ pages/registrarse.php
@@ -4,17 +4,19 @@ include '../util/conexion.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrarse</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"> 
-    <link rel="stylesheet" href="../styles/index.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registrarse</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../styles/index.css">
 </head>
+
 <body>
-    <?php
-        include '../util/menu.php';
- ?>
+  <?php
+  include '../util/menu.php';
+  ?>
 
 
 
@@ -48,10 +50,11 @@ include '../util/conexion.php';
     </div>
   </div>
 
-  
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../js/registrarse.js"></script>
-    
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="../js/registrarse.js"></script>
+
 </body>
+
 </html>
