2d9c93fdf62b3dcc0973e3c6a9269c21884cd883
Steeven Toala <steevenpfb@gmail.com>
2025-01-11 21:34:11 -0500

    Menu dev

--- employee/gestion_returns.php
+++ employee/gestion_returns.php
@@ -68,9 +68,14 @@ if ($result->num_rows > 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../styles/footer.css">
-    <title>Gestión de Devoluciones</title>
-    <link rel="stylesheet" href="../styles/footer.css">
+
+    
+    
+    <title>Gestión de Devoluciones</title>    
 
 
     <link rel="stylesheet" href="../styles/home_admin.css">
@@ -273,6 +278,7 @@ if ($result->num_rows > 0) {
     include './home_section/modals/contrato.html';
     include './home_section/modals/modal_editDev.html';
     include './home_section/modals/devAdd.html';
+    include './home_section/scripts/menu.php'
     ?>
 
     <div class="container mt-4">
